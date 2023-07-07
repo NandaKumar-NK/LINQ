06e5bb638bcd503d994cb6bfe0521428bbd9df61
NandaKumar-NK <nanda.kanini@gmail.com>
2023-07-07 16:09:00 +0530

    work

---
+++ LINQ TO SQL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_TO_SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataClasses1DataContext dt=new DataClasses1DataContext();
+        }
+    }
+}
